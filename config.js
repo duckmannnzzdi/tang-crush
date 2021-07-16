4d2000e7e580603044bc15b5a0f7089148665571
duckmannnzzdi <87535099+duckmannnzzdi@users.noreply.github.com>
2021-07-16 21:36:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     introTitle: 'Nhiều chút nhớ bé nên nghịch mụt xíu',
-    introDesc: `Tuy là mình đang hơi xa nhau mụt chút nhưng mà trái tym và tâm trí anh chỉ có mỗi bé hoiiii`,
+    introDesc: `Tuy là mình đang hơi xa nhau mụt chút nhưng mà trái tym và tâm trí anh chỉ có mỗi bé hoiiii.`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
@@ -10,7 +10,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Luông nhớ với yêu anh bé nhó ',
+    messDesc: 'Luông nhớ với yêu anh bé nhó.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
